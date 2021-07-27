bc9397d73544e98172dd170fdf9fb97af60ad1be
hoaithuong2002 <dgth2o2@gmail.com>
2021-07-27 10:06:20 +0700

    create list product

--- demo-git/src/app/app.module.ts
+++ demo-git/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListProductComponent } from './component/list-product/list-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListProductComponent
   ],
   imports: [
     BrowserModule,
